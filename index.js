0f992cb9e49d0f2bb61cb755983de32377a63cd8
bhanu chandra <80154731+bhanuylm75@users.noreply.github.com>
2024-10-06 21:34:29 +0530

    Your commit message

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import User from "./models/usermodel.js"
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.use(express.json());
 app.use(cors())
 app.use(cookieParser())
